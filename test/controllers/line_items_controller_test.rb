13280f91c48434accce3b4ef3711722051949f5c
katenorquay <katenorquay@live.com>
2017-04-04 16:58:34 +1200

    adds styling when a new item is added to the cartand tests line items are created with ajax

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -43,6 +43,18 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
      assert_response :success
   end
 
+  test "should create line_item via ajax" do
+  assert_difference('LineItem.count') do
+    post line_items_url, params: { product_id: products(:ruby).id },
+      xhr: true
+  end
+
+  assert_response :success
+  assert_select_jquery :html, '#cart' do
+  assert_select 'tr#current_item td', /Programming Ruby 1.9/
+  end
+end
+
   test "should destroy line_item" do
     assert_difference('LineItem.count', -1) do
       delete line_item_url(@line_item)
